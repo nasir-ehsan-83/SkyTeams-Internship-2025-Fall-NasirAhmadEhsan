4df3f876efe6d31fb448bc98a5754188bb7cb407
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-08-23 17:17:31 +0430

    feat: add update_streak_service for streak management
    - Add update_streak_service function
    - Handle streak creation, increment, and reset logic
    - Add error handling and logging

--- habit-tracker/app/services/streaks_service.py
+++ habit-tracker/app/services/streaks_service.py
@@ -3,6 +3,7 @@ from fastapi import (
     HTTPException,
     status
 )
+from datetime import date
 
 from app.config import logger
 from app.models import Streak
@@ -87,6 +88,67 @@ async def get_best_streak_service(
     
     except Exception as error:
         logger.error(f"Unexpected error in get_best_streak_service: {error}", exc_info = True)
+        
+        raise HTTPException(
+            status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail = "Internal server error"
+        )
+
+
+
+
+async def update_streak_service(
+    owner_id:   BeanieObjectId,
+    habit_id:   BeanieObjectId,
+    track_date: date
+) -> None:
+    
+    try:
+        streak = await Streak.find_one({
+            "owner_id": owner_id,
+            "habit_id": habit_id
+        })
+        
+        if not streak:
+            new_streak = Streak(
+                owner_id = owner_id,
+                habit_id = habit_id,
+                current_streak = 1,
+                longest_streak = 1,
+                start_date = track_date,
+                last_tracked = track_date,
+                status = "active"
+            )
+            await new_streak.insert() # type: ignore
+            return
+        
+        if streak.last_tracked:
+            day_diff = (track_date - streak.last_tracked).days
+        else:
+            day_diff = 1
+        
+        if day_diff == 1:
+            streak.current_streak += 1
+            streak.last_tracked = track_date
+            
+            if streak.current_streak > streak.longest_streak:
+                streak.longest_streak = streak.current_streak
+                
+        elif day_diff > 1:
+            streak.current_streak = 1
+            streak.start_date = track_date
+            streak.last_tracked = track_date
+            
+        else:
+            return
+        
+        await streak.save() # type: ignore
+        
+    except HTTPException:
+        raise
+    
+    except Exception as error:
+        logger.error(f"Unexpected error in update_streak_service: {error}", exc_info = True)
         
         raise HTTPException(
             status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
